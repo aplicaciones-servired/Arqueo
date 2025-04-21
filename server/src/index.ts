21b6bc9201f82107c80ac09e04a852f7e4954da2
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-04-21 15:05:44 -0500

    prueba

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import { PORT } from './config';
 import { arqueoRoute } from './routes/arqueo.routes';
 import { loginRouter } from './routes/users.routes';
-
 import express from 'express';
 import log from 'morgan';
 import cors from 'cors';
@@ -12,11 +11,10 @@ app.use(express.json());
 app.use(cors());
 app.use(log('dev'));
 
-app.use(arqueoRoute);
-app.use('/', loginRouter );
+// Agrega el prefijo /api a todas las rutas
+app.use('/api', arqueoRoute);   // Ahora las rutas serán /api/arqueo/...
+app.use('/api', loginRouter);   // Ahora las rutas serán /api/login/...
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
-
-
