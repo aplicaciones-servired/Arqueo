0182a836704104e9ed0c972481cc0b9eb9f64222
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-04-22 10:55:04 -0500

    .

--- server/src/connections/dbArqueo.ts
+++ server/src/connections/dbArqueo.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Dialect } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,11 +7,11 @@ const DB_ARQUEO_HOST = process.env.DB_SERVIRED_HOST as string
 const DB_ARQUEO_PASSWORD = process.env.DB_SERVIRED_PASS as string
 const DB_ARQUEO_USER = process.env.DB_SERVIRED_USER as string
 const DB_ARQUEO_NAME = process.env.DB_SERVIRED_USE as string
-const DB_DIALECT_ARQUEO = process.env.DB_DIALECT_ARQUEO as string
+const DB_DIALECT_CHAT = process.env.DB_DIALECT_ARQUEO as string
 
 const getPoolArqueo = new Sequelize(DB_ARQUEO_NAME, DB_ARQUEO_USER, DB_ARQUEO_PASSWORD, {
   host: DB_ARQUEO_HOST,
-  dialect: "mysql",
+  dialect: DB_DIALECT_CHAT as Dialect,
   timezone: '-05:00',
 });
 
