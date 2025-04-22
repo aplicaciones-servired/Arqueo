cc1299a8edc089633cccebcd6ff7af00364e0c0b
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-04-22 09:24:51 -0500

    inicio

--- client/src/services/Login.services.ts
+++ client/src/services/Login.services.ts
@@ -2,8 +2,7 @@ import axios, { type AxiosResponse } from 'axios'
 import { API_URL } from '../utils/constans'
 export const getLogin = async ({ username, password }: { username: string, password: string }): Promise<unknown> => {
   try {
-    // const response: AxiosResponse<unknown> = await axios.post(`${API_URL}/login`, { username, password })
-    const response: AxiosResponse<{ token: string }> = await axios.post(`${API_URL}/login`, { username, password })
+    const response: AxiosResponse<unknown> = await axios.post(`${API_URL}/login`, { username, password })
     // const response: AxiosResponse<{ token: string }> = await axios.post('http://localhost:3000/login', { username, password })
     return response.data
   } catch (error) {
