4351445e533bd07a2b9f5b390d5efb50d481f209
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-04-23 09:41:54 -0500

    cambio

--- server/src/connections/dbLogin.ts
+++ server/src/connections/dbLogin.ts
@@ -1,34 +1,23 @@
+import { DB_SERVIRE_HOST, DB_SERVIRE_USE, DB_SERVIRE_PASS, DB_PORT, DB_SERVIRE_USER } from '../config/index';
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const DB_HOST_LOGIN = process.env.DB_SERVIRE_HOST as string
-const DB_PASSWORD_LOGIN = process.env.DB_SERVIRE_PASS as string
-const DB_USER_LOGIN = process.env.DB_SERVIRE_USER as string
-const DB_NAME_LOGIN = process.env.DB_SERVIRE_USE as string
-const DB_DIALECT_CHAT = process.env.DB_DIALECT_CHAT as string
 
 console.log({
-  DB_USER_LOGIN: process.env.DB_SERVIRE_USER,
-  DB_PASSWORD_LOGIN: process.env.DB_SERVIRE_PASS,
-  DB_HOST_LOGIN: process.env.DB_SERVIRE_HOST,
-  DB_NAME_LOGIN: process.env.DB_SERVIRE_USE
+  DB_SERVIRE_USER,
+  DB_SERVIRE_PASS,
+  DB_SERVIRE_HOST,
+  DB_SERVIRE_USE,
+  DB_PORT
 });
 
-
-console.log('Usuario:', process.env.DB_SERVIRE_USER);
-console.log('Pass:', process.env.DB_SERVIRE_PASS);
-console.log('Host:', process.env.DB_SERVIRE_HOST);
-console.log('DB:', process.env.DB_SERVIRE_USE);
-
-
-const getPoolLogin = new Sequelize(DB_NAME_LOGIN, DB_USER_LOGIN, DB_PASSWORD_LOGIN, {
-  host: DB_HOST_LOGIN,
-  dialect: "mysql",
-  port: 3306,
+const getPoolLogin = new Sequelize(DB_SERVIRE_USE, DB_SERVIRE_USER, DB_SERVIRE_PASS, {
+  host: DB_SERVIRE_HOST,
+  port: DB_PORT,
+  dialect: 'mysql',
   timezone: '-05:00',
-  
+  logging: false
 });
 
-export { getPoolLogin };
+export { getPoolLogin }
+
+
+
