98f03bb80b3dd8ba8097b7bd20bb403101dd3634
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-04-23 08:49:56 -0500

    db

--- server/src/connections/dbLogin.ts
+++ server/src/connections/dbLogin.ts
@@ -10,11 +10,17 @@ const DB_NAME_LOGIN = process.env.DB_SERVIRE_USE as string
 const DB_DIALECT_CHAT = process.env.DB_DIALECT_CHAT as string
 
 console.log({
-  DB_USER_LOGIN,
-  DB_PASSWORD_LOGIN,
-  DB_HOST_LOGIN,
-  DB_NAME_LOGIN
+  DB_USER_LOGIN: process.env.DB_SERVIRE_USER,
+  DB_PASSWORD_LOGIN: process.env.DB_SERVIRE_PASS,
+  DB_HOST_LOGIN: process.env.DB_SERVIRE_HOST,
+  DB_NAME_LOGIN: process.env.DB_SERVIRE_USE
 });
+
+
+console.log('Usuario:', process.env.DB_SERVIRE_USER);
+console.log('Pass:', process.env.DB_SERVIRE_PASS);
+console.log('Host:', process.env.DB_SERVIRE_HOST);
+console.log('DB:', process.env.DB_SERVIRE_USE);
 
 
 const getPoolLogin = new Sequelize(DB_NAME_LOGIN, DB_USER_LOGIN, DB_PASSWORD_LOGIN, {
@@ -22,6 +28,7 @@ const getPoolLogin = new Sequelize(DB_NAME_LOGIN, DB_USER_LOGIN, DB_PASSWORD_LOG
   dialect: "mysql",
   port: 3306,
   timezone: '-05:00',
+  
 });
 
 export { getPoolLogin };
