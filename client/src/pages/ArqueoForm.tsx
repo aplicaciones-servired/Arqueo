b805106009de514140f35b450856683e777d7d71
aplicaciones-servired <158181227+aplicaciones-servired@users.noreply.github.com>
2025-05-26 15:42:50 -0500

    Update ArqueoForm.tsx

--- client/src/pages/ArqueoForm.tsx
+++ client/src/pages/ArqueoForm.tsx
@@ -29,7 +29,7 @@ const ArqueoForm = (): JSX.Element => {
             ...item,
             firma_auditoria: item.firma_auditoria != null ? Buffer.from(item.firma_auditoria as string).toString('base64') : null,
             firma_colocadora: item.firma_colocadora != null ? Buffer.from(item.firma_colocadora as string).toString('base64') : null,
-            imagen_observacion: item.imagen_observacion != null ? Buffer.from(item.imagen_observacion as string).toString() : null
+            imagen_observacion: item.imagen_observacion != null ? Buffer.from(item.imagen_observacion as string).toString('base64') : null,
           }))
 
           setData(dataWithBase64Images as Arqueo[])
@@ -2152,10 +2152,10 @@ const ArqueoForm = (): JSX.Element => {
     <>
       <h4 className="block uppercase mb-2">Firma Auditoria</h4>
       <img
-        src={`data:image/png;base64,${arqueo.firma_auditoria}`}
-        className="w-30 h-20 object-contain"
-        alt="Firma Auditoria"
-      />
+  src={`data:image/png;base64,${arqueo.firma_auditoria}`}
+  className="w-30 h-20 object-contain"
+  alt="Firma Auditoria"
+/>
     </>
   )}
               </div>
